db785ba2a5c6942517c04a8a58b6e86d597eea48
Kustov Ilya <kusgent@yandex.ru>
2021-07-28 22:38:50 +0300

    added solution lesson1_step05.py

---
+++ section2/lesson1_step05.py
@@ -0,0 +1,33 @@
+# https://stepik.org/lesson/165493/step/5?unit=140087
+
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    ans = browser.find_element_by_id("answer")
+    ans.send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(5)
+    browser.quit()
