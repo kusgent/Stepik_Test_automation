06b69c6518093320e8fce62675df18cc8b9b91c2
Kustov Ilya <kusgent@yandex.ru>
2021-08-01 21:40:00 +0300

    added solution lesson2_step06.py

---
+++ section2/lesson2_step06.py
@@ -0,0 +1,35 @@
+# https://stepik.org/lesson/228249/step/6?unit=200781
+
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    browser.execute_script("window.scrollBy(0, 150);")
+
+    ans = browser.find_element_by_id("answer")
+    ans.send_keys(y)
+
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(5)
+    browser.quit()
